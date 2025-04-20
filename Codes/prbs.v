d742343cddfaf226c7997cdd64f9a7f75ed2d2fb
ALONE-21 <124436664+ALONE-21@users.noreply.github.com>
2025-04-21 02:53:54 +0530

    Update prbs.v

--- Codes/prbs.v
+++ Codes/prbs.v
@@ -1,1 +1,8 @@
 // Module definition for prbs.v
+module prbs(input clk, input rst, output reg out);
+    reg [6:0] shift = 7'b1000000;
+    always @(posedge clk or posedge rst)
+        if (rst) shift <= 7'b1000000;
+        else shift <= {shift[5:0], shift[6] ^ shift[5]};
+    assign out = shift[6];
+endmodule
