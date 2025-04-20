1ced74d25d51f0cd2ec02e6480b8ccc8fec11886
ALONE-21 <124436664+ALONE-21@users.noreply.github.com>
2025-04-21 02:53:27 +0530

    Update accumulator.v

--- Codes/accumulator.v
+++ Codes/accumulator.v
@@ -1,1 +1,6 @@
 // Module definition for accumulator.v
+module accumulator(input clk, input rst, input [7:0] data_in, input load, output reg [7:0] acc);
+    always @(posedge clk or posedge rst)
+        if (rst) acc <= 0;
+        else if (load) acc <= acc + data_in;
+endmodule
