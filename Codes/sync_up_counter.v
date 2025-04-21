31a3c7f1ab7d6e434aba007d0fbedfc5ffc9d718
ALONE-21 <124436664+ALONE-21@users.noreply.github.com>
2025-04-21 06:27:39 +0530

    Update sync_up_counter.v

--- Codes/sync_up_counter.v
+++ Codes/sync_up_counter.v
@@ -1,1 +1,6 @@
 // Module definition for sync_up_counter.v
+module sync_up_counter(input clk, input rst, output reg [3:0] Q);
+    always @(posedge clk or posedge rst)
+        if (rst) Q <= 0;
+        else Q <= Q + 1;
+endmodule
