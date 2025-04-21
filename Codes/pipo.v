57ce45583ad01797df8168beb08667661e8da3f3
ALONE-21 <124436664+ALONE-21@users.noreply.github.com>
2025-04-21 06:26:38 +0530

    Update pipo.v

--- Codes/pipo.v
+++ Codes/pipo.v
@@ -1,1 +1,6 @@
 // Module definition for pipo.v
+module pipo(input clk, input rst, input [7:0] din, output reg [7:0] dout);
+    always @(posedge clk or posedge rst)
+        if (rst) dout <= 8'b0;
+        else dout <= din;
+endmodule
