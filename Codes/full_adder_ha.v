b2df251753fe6408b254910a406431005b05c7d0
ALONE-21 <124436664+ALONE-21@users.noreply.github.com>
2025-04-21 06:15:26 +0530

    Update full_adder_ha.v

--- Codes/full_adder_ha.v
+++ Codes/full_adder_ha.v
@@ -1,1 +1,12 @@
 // Module definition for full_adder_ha.v
+module half_adder(input A, input B, output SUM, output CARRY);
+    assign SUM = A ^ B;
+    assign CARRY = A & B;
+endmodule
+
+module full_adder_ha(input A, input B, input Cin, output SUM, output CARRY);
+    wire s1, c1, c2;
+    half_adder HA1 (.A(A), .B(B), .SUM(s1), .CARRY(c1));
+    half_adder HA2 (.A(s1), .B(Cin), .SUM(SUM), .CARRY(c2));
+    assign CARRY = c1 | c2;
+endmodule
