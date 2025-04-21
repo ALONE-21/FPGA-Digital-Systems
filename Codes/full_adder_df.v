0bc9c620adb4bfe6c2c70d20b60a734d06999f39
ALONE-21 <124436664+ALONE-21@users.noreply.github.com>
2025-04-21 06:13:33 +0530

    Update full_adder_df.v

--- Codes/full_adder_df.v
+++ Codes/full_adder_df.v
@@ -1,1 +1,5 @@
 // Module definition for full_adder_df.v
+module full_adder_df(input A, input B, input Cin, output SUM, output CARRY);
+    assign SUM = A ^ B ^ Cin;
+    assign CARRY = (A & B) | (B & Cin) | (A & Cin);
+endmodule
