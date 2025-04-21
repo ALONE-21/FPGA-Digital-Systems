4613a796d793d4cb5d59a78040403139a62798d7
ALONE-21 <124436664+ALONE-21@users.noreply.github.com>
2025-04-21 06:28:05 +0530

    Update sync_updown_counter.v

--- Codes/sync_updown_counter.v
+++ Codes/sync_updown_counter.v
@@ -1,1 +1,7 @@
 // Module definition for sync_updown_counter.v
+module sync_updown_counter(input clk, input rst, input up, output reg [3:0] Q);
+    always @(posedge clk or posedge rst)
+        if (rst) Q <= 0;
+        else if (up) Q <= Q + 1;
+        else Q <= Q - 1;
+endmodule
