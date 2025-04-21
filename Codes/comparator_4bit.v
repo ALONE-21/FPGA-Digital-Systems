af5c0cb3ae34ffb71dcf8411d3497ee09188bbe7
ALONE-21 <124436664+ALONE-21@users.noreply.github.com>
2025-04-21 06:22:09 +0530

    Update comparator_4bit.v

--- Codes/comparator_4bit.v
+++ Codes/comparator_4bit.v
@@ -1,1 +1,6 @@
 // Module definition for comparator_4bit.v
+module comparator_4bit(input [3:0] A, input [3:0] B, output A_gt_B, output A_eq_B, output A_lt_B);
+    assign A_gt_B = (A > B);
+    assign A_eq_B = (A == B);
+    assign A_lt_B = (A < B);
+endmodule
