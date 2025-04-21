f47a7383b78f0c44047bbd5c1c8bbbd24b6b8b38
ALONE-21 <124436664+ALONE-21@users.noreply.github.com>
2025-04-21 06:27:52 +0530

    Update sync_down_counter.v

--- Codes/sync_down_counter.v
+++ Codes/sync_down_counter.v
@@ -1,1 +1,6 @@
 // Module definition for sync_down_counter.v
+module sync_down_counter(input clk, input rst, output reg [3:0] Q);
+    always @(posedge clk or posedge rst)
+        if (rst) Q <= 4'b1111;
+        else Q <= Q - 1;
+endmodule
