ccbb61e949815292c3e20bf3b11897f96a1b9e63
ALONE-21 <124436664+ALONE-21@users.noreply.github.com>
2025-04-21 06:26:26 +0530

    Update piso.v

--- Codes/piso.v
+++ Codes/piso.v
@@ -1,1 +1,9 @@
 // Module definition for piso.v
+module piso(input clk, input rst, input load, input [7:0] din, output dout);
+    reg [7:0] shift;
+    always @(posedge clk or posedge rst)
+        if (rst) shift <= 8'b0;
+        else if (load) shift <= din;
+        else shift <= {shift[6:0], 1'b0};
+    assign dout = shift[7];
+endmodule
