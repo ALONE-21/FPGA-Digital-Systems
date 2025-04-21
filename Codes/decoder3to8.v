f3c14ac5b5469de48bd1c3e0efec988d0e673c01
ALONE-21 <124436664+ALONE-21@users.noreply.github.com>
2025-04-21 06:18:23 +0530

    Update decoder3to8.v

--- Codes/decoder3to8.v
+++ Codes/decoder3to8.v
@@ -1,1 +1,7 @@
 // Module definition for decoder3to8.v
+module decoder3to8(input [2:0] A, output reg [7:0] Y);
+    always @(A) begin
+        Y = 8'b00000000;
+        Y[A] = 1'b1;
+    end
+endmodule
