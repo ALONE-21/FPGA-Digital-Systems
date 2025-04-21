0392cc69ed582e6f0771b7eeef7a62f0549203d5
ALONE-21 <124436664+ALONE-21@users.noreply.github.com>
2025-04-21 06:26:57 +0530

    Update universal_shift_reg.v

--- Codes/universal_shift_reg.v
+++ Codes/universal_shift_reg.v
@@ -1,1 +1,16 @@
 // Module definition for universal_shift_reg.v
+module universal_shift_reg(
+    input clk, input rst, input [1:0] mode,
+    input [7:0] din, input sin,
+    output reg [7:0] dout);
+    always @(posedge clk or posedge rst)
+        if (rst) dout <= 0;
+        else begin
+            case(mode)
+                2'b00: dout <= dout; // Hold
+                2'b01: dout <= {dout[6:0], sin}; // Shift Left
+                2'b10: dout <= {sin, dout[7:1]}; // Shift Right
+                2'b11: dout <= din; // Parallel Load
+            endcase
+        end
+endmodule
