7cd34b0e6e71f131f8093066527be97d991532d8
ALONE-21 <124436664+ALONE-21@users.noreply.github.com>
2025-04-21 06:23:57 +0530

    Update t_latch.v

--- Codes/t_latch.v
+++ Codes/t_latch.v
@@ -1,1 +1,8 @@
 // Module definition for t_latch.v
+module t_latch(input T, input EN, output reg Q);
+    always @(EN or T) begin
+        if (EN) begin
+            if (T) Q <= ~Q;
+        end
+    end
+endmodule
