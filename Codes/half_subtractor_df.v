14ca0c7059bdbba004031060b4eb2d384f13149a
ALONE-21 <124436664+ALONE-21@users.noreply.github.com>
2025-04-21 06:16:12 +0530

    Update half_subtractor_df.v

--- Codes/half_subtractor_df.v
+++ Codes/half_subtractor_df.v
@@ -1,1 +1,5 @@
 // Module definition for half_subtractor_df.v
+module half_subtractor_df(input A, input B, output DIFF, output BORROW);
+    assign DIFF = A ^ B;
+    assign BORROW = ~A & B;
+endmodule
