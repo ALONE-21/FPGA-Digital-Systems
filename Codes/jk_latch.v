87c63b09a38dc2d4dd683b6357499eb46c468c7f
ALONE-21 <124436664+ALONE-21@users.noreply.github.com>
2025-04-21 06:23:39 +0530

    Update jk_latch.v

--- Codes/jk_latch.v
+++ Codes/jk_latch.v
@@ -1,1 +1,11 @@
 // Module definition for jk_latch.v
+module jk_latch(input J, input K, input EN, output reg Q);
+    always @(EN or J or K) begin
+        if (EN) begin
+            if (~J & ~K) Q <= Q;
+            else if (~J & K) Q <= 0;
+            else if (J & ~K) Q <= 1;
+            else Q <= ~Q;
+        end
+    end
+endmodule
