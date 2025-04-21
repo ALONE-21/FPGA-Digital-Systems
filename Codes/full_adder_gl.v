2696af27aa9213f2b1f77b7d7b9450332f451c97
ALONE-21 <124436664+ALONE-21@users.noreply.github.com>
2025-04-21 06:15:05 +0530

    Update full_adder_gl.v

--- Codes/full_adder_gl.v
+++ Codes/full_adder_gl.v
@@ -1,1 +1,11 @@
 // Module definition for full_adder_gl.v
+module full_adder_gl(input A, input B, input Cin, output SUM, output CARRY);
+    wire w1, w2, w3, w4, w5;
+    xor(w1, A, B);
+    xor(SUM, w1, Cin);
+    and(w2, A, B);
+    and(w3, B, Cin);
+    and(w4, A, Cin);
+    or(w5, w2, w3);
+    or(CARRY, w5, w4);
+endmodule
