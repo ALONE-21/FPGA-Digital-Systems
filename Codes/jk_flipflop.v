5d6dbbf971ff6f48085e0635a2abfbb2829c8433
ALONE-21 <124436664+ALONE-21@users.noreply.github.com>
2025-04-21 06:25:09 +0530

    Update jk_flipflop.v

--- Codes/jk_flipflop.v
+++ Codes/jk_flipflop.v
@@ -1,1 +1,10 @@
 // Module definition for jk_flipflop.v
+module jk_flipflop(input J, input K, input CLK, output reg Q);
+    always @(posedge CLK)
+        case({J,K})
+            2'b00: Q <= Q;
+            2'b01: Q <= 0;
+            2'b10: Q <= 1;
+            2'b11: Q <= ~Q;
+        endcase
+endmodule
