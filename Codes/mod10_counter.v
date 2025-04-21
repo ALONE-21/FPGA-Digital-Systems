3a837f52a112dac61c0e14476731fdd3f111f090
ALONE-21 <124436664+ALONE-21@users.noreply.github.com>
2025-04-21 06:28:23 +0530

    Update mod10_counter.v

--- Codes/mod10_counter.v
+++ Codes/mod10_counter.v
@@ -1,1 +1,7 @@
 // Module definition for mod10_counter.v
+module mod10_counter(input clk, input rst, output reg [3:0] Q);
+    always @(posedge clk or posedge rst)
+        if (rst) Q <= 0;
+        else if (Q == 9) Q <= 0;
+        else Q <= Q + 1;
+endmodule
