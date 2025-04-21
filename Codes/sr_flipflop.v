fd4fbb8f39997f227c36a2adc23ac0d7bbb310fe
ALONE-21 <124436664+ALONE-21@users.noreply.github.com>
2025-04-21 06:24:53 +0530

    Update sr_flipflop.v

--- Codes/sr_flipflop.v
+++ Codes/sr_flipflop.v
@@ -1,1 +1,10 @@
 // Module definition for sr_flipflop.v
+module sr_flipflop(input S, input R, input CLK, output reg Q);
+    always @(posedge CLK)
+        case({S,R})
+            2'b00: Q <= Q;
+            2'b01: Q <= 0;
+            2'b10: Q <= 1;
+            2'b11: Q <= 1'bx;
+        endcase
+endmodule
