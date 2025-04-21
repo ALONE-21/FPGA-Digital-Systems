4d7b0a19195de3f65967166b7114b860546d5873
ALONE-21 <124436664+ALONE-21@users.noreply.github.com>
2025-04-21 06:20:57 +0530

    Update demux1to8.v

--- Codes/demux1to8.v
+++ Codes/demux1to8.v
@@ -1,1 +1,7 @@
 // Module definition for demux1to8.v
+module demux1to8(input D, input [2:0] S, output reg [7:0] Y);
+    always @(*) begin
+        Y = 8'b00000000;
+        Y[S] = D;
+    end
+endmodule
