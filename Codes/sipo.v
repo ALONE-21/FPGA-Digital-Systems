d9e31c739c2adaa7990271a23ab48fd59ae9093c
ALONE-21 <124436664+ALONE-21@users.noreply.github.com>
2025-04-21 06:26:12 +0530

    Update sipo.v

--- Codes/sipo.v
+++ Codes/sipo.v
@@ -1,1 +1,8 @@
 // Module definition for sipo.v
+module sipo(input clk, input rst, input din, output [7:0] dout);
+    reg [7:0] shift;
+    always @(posedge clk or posedge rst)
+        if (rst) shift <= 8'b0;
+        else shift <= {shift[6:0], din};
+    assign dout = shift;
+endmodule
