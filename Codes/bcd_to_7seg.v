80ebdd402167fb0368723b8588b0d7e264ec9c46
ALONE-21 <124436664+ALONE-21@users.noreply.github.com>
2025-04-21 06:21:15 +0530

    Update bcd_to_7seg.v

--- Codes/bcd_to_7seg.v
+++ Codes/bcd_to_7seg.v
@@ -1,1 +1,18 @@
 // Module definition for bcd_to_7seg.v
+module bcd_to_7seg(input [3:0] BCD, output reg [6:0] SEG);
+    always @(*) begin
+        case (BCD)
+            4'd0: SEG = 7'b1000000;
+            4'd1: SEG = 7'b1111001;
+            4'd2: SEG = 7'b0100100;
+            4'd3: SEG = 7'b0110000;
+            4'd4: SEG = 7'b0011001;
+            4'd5: SEG = 7'b0010010;
+            4'd6: SEG = 7'b0000010;
+            4'd7: SEG = 7'b1111000;
+            4'd8: SEG = 7'b0000000;
+            4'd9: SEG = 7'b0010000;
+            default: SEG = 7'b1111111;
+        endcase
+    end
+endmodule
