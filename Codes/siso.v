08c8931f8ee627adb00f53a63842514e94c1b5cd
ALONE-21 <124436664+ALONE-21@users.noreply.github.com>
2025-04-21 06:26:01 +0530

    Update siso.v

--- Codes/siso.v
+++ Codes/siso.v
@@ -1,1 +1,8 @@
 // Module definition for siso.v
+module siso(input clk, input rst, input din, output dout);
+    reg [7:0] shift;
+    always @(posedge clk or posedge rst)
+        if (rst) shift <= 8'b0;
+        else shift <= {shift[6:0], din};
+    assign dout = shift[7];
+endmodule
