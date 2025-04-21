27b8a99b07c757905122be62689bda94e22c3c1d
ALONE-21 <124436664+ALONE-21@users.noreply.github.com>
2025-04-21 06:16:48 +0530

    Update full_subtractor_df.v

--- Codes/full_subtractor_df.v
+++ Codes/full_subtractor_df.v
@@ -1,1 +1,5 @@
 // Module definition for full_subtractor_df.v
+module full_subtractor_df(input A, input B, input Bin, output DIFF, output BORROW);
+    assign DIFF = A ^ B ^ Bin;
+    assign BORROW = (~A & B) | (~(A ^ B) & Bin);
+endmodule
