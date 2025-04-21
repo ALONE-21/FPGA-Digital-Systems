da51444a707bbc056a3875009c2f2e5c27794702
ALONE-21 <124436664+ALONE-21@users.noreply.github.com>
2025-04-21 06:17:03 +0530

    Update full_subtractor_gl.v

--- Codes/full_subtractor_gl.v
+++ Codes/full_subtractor_gl.v
@@ -1,1 +1,9 @@
 // Module definition for full_subtractor_gl.v
+module full_subtractor_gl(input A, input B, input Bin, output DIFF, output BORROW);
+    wire d1, b1, b2, b3;
+    xor(d1, A, B);
+    xor(DIFF, d1, Bin);
+    and(b1, ~A, B);
+    and(b2, ~d1, Bin);
+    or(BORROW, b1, b2);
+endmodule
